ae29495a215a8663bd678ebaa9a9dc90bf9c0fac
titainis <titukas@gmail.com>
2026-04-27 11:00:37 +0300

    Initial commit: Add React Vite project with Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to DBVS Karate Varzybos</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
